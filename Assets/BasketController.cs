2620ff44972b9d419cc3ed99dc5766b59dc10df5
wendycchen <55068125+wendycchen@users.noreply.github.com>
2019-09-08 18:50:33 -0600

    Add files via upload

---
+++ Assets/BasketController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketController : MonoBehaviour
+{
+    public AudioClip appleSE;
+    public AudioClip bombSE;
+    AudioSource aud;
+    GameObject director;
+
+    void Start()
+    {
+        this.director = GameObject.Find("GameDirector");
+        this.aud = GetComponent<AudioSource>();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Apple")
+        {
+            this.director.GetComponent<GameDirector>().getApple();
+            this.aud.PlayOneShot(this.appleSE);
+        } else
+        {
+            this.director.GetComponent<GameDirector>().getBomb();
+            this.aud.PlayOneShot(this.bombSE);
+        }
+        Destroy(other.gameObject); 
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); //input.mousePosition = touching coordinates from mouse
+            RaycastHit hit; //RayCast checks if beam of light touches the stage
+            if(Physics.Raycast(ray, out hit, Mathf.Infinity)) //out is the value being returned on the coords
+            {
+                float x = Mathf.RoundToInt(hit.point.x); 
+                float z = Mathf.RoundToInt(hit.point.z);
+                transform.position = new Vector3(x, 0, z);
+            }
+        }
+    }
+}
---
+++ Assets/ItemController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float dropSpeed = -0.03f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, this.dropSpeed, 0);
+        if (transform.position.y < -0.6f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
